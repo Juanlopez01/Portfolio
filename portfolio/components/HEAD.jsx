5e62fd8fee4daa5ba9ef9284d1d58e7609bdfe79
Juan Lopez <juanpablolopez43@gmail.com>
2022-12-29 11:02:21 -0300

    two

---
+++ portfolio/components/HEAD.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import React from 'react'
+
+const HEAD = () => {
+  return (
+    <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Hind:wght@300&family=Montserrat&display=swap" rel="stylesheet"/>
+
+    </Head>
+  )
+}
+
+export default HEAD
---
+++ portfolio/components/hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from '../styles/hero.module.css';
+
+
+const Hero = () => {
+  return (
+    <div>
+        <div>
+            <h1>Juan Pablo Lopez</h1>
+            <p>
+              Full Stack Developer
+            </p>
+        </div>
+        <div>
+          <img src='https://github.com/Juanlopez01/portfolio/blob/main/portfolio/images/JPL.jpeg' alt="Juan Pablo Lopez" />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
